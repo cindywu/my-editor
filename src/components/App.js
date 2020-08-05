25a03a37ed86a87429768bfe7dc422e9a5b4a5d5
cindy <ciindy.wu@gmail.com>
2020-08-04 22:26:36 -1000

    adds pm code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react';
-import {EditorState} from "prosemirror-state"
+
+import {EditorState} from "./pm-state/state"
+import {schema} from "./pm-schema-basic/schema-basic"
+import {exampleSetup} from "./pm-example-setup/index"
+import {addListNodes} from "./pm-schema-list/schema-list"
+import {Schema, DOMParser} from "./pm-model/index"
+
 import {EditorView} from "prosemirror-view"
-import {Schema, DOMParser} from "prosemirror-model"
-import {schema} from "prosemirror-schema-basic"
-import {addListNodes} from "prosemirror-schema-list"
-import {exampleSetup} from "prosemirror-example-setup"
+
+
 import '../css/app.css'
 
 function App() {
@@ -20,12 +24,31 @@ function App() {
         plugins: exampleSetup({schema: mySchema})
       })
     })
+
+    const mySchema2 = new Schema({
+      nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+      marks: schema.spec.marks
+    })
+
+    window.view = new EditorView(document.querySelector("#editor2"), {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema2).parse(document.querySelector("#content2")),
+        plugins: exampleSetup({schema: mySchema2})
+      })
+    })
   })
 
   return (
     <div className="App">
-      <div id="editor" />
-      <div id="content" />
+      <div>
+        <div id="editor" />
+        <div id="content" />
+      </div>
+      <div>
+        <div id="editor2" />
+        <div id="content2" />
+      </div>
+      Hello
     </div>
   )
 }
